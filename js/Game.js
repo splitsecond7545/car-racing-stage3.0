bf9bd4824881d8824ae56ad06da959fb51f27743
Yash <splitsecond7545@gmail.com>
2021-02-21 14:56:01 -0600

    commit statement

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,17 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1_img);
+
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2_img);
+
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3_img);
+
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4_img);
+
     cars = [car1, car2, car3, car4];
   }
 
@@ -43,12 +51,13 @@ class Game {
     
     if(allPlayers !== undefined){
       //var display_position = 100;
-      
+      background('#c68767');
+      image(track, 0, -displayHeight*4, displayWidth, displayHeight*5);
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 175;
       var y;
 
       for(var plr in allPlayers){
@@ -78,7 +87,13 @@ class Game {
       player.distance +=10
       player.update();
     }
-
+    if(player.distance>3860){
+      gameState = 2;
+    }
     drawSprites();
   }
+  end(){
+    console.log("Game Ended");
+  }
+
 }
